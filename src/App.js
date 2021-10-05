88d375b34bc6ae29b7ac71e8aefbad040b7d5eab
Rishabh Tater <rishabhtater14@gmail.com>
2021-10-05 22:47:55 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -23,26 +23,11 @@ function App() {
         <Route exact path='/data-cables' component={DCHomepage} />
         <Route exact path='/chargers' component={ChargersHomepage} />
         <Route exact path='/:productCatagory/:productId' component={FullPageDescription} />
-        {/* <Route exact path='/data-cables/dc2' component={DCHomepage} />
-        <Route exact path='/data-cables/dc3' component={DCHomepage} />
-        <Route exact path='/chargers/ch1' component={ChargersHomepage} />
-        <Route exact path='/chargers/ch2' component={ChargersHomepage} /> */}
         <Route exact path='/handsfree' component={HFHomepage} />
-        {/* <Route exact path='/handsfree/hf1' component={ChargersHomepage} />
-        <Route exact path='/handsfree/hf2' component={ChargersHomepage} /> */}
+        <Route exact path='/handsfree/hf2' component={ChargersHomepage} />
         <Route exact path='/microSD' component={MicroSDHomepage} />
-        {/* <Route exact path='/microSD/sd1' component={MicroSDHomepage} />
-        <Route exact path='/microSD/sd2' component={MicroSDHomepage} />
-        <Route exact path='/microSD/sd3' component={MicroSDHomepage} />
-        <Route exact path='/microSD/sd4' component={MicroSDHomepage} /> */}
         <Route exact path='/pendrive' component={PenDriveHomepage} />
-        {/* <Route exact path='/pendrive/pd1' component={PenDriveHomepage} />
-        <Route exact path='/pendrive/pd2' component={PenDriveHomepage} />
-        <Route exact path='/pendrive/pd3' component={PenDriveHomepage} />
-        <Route exact path='/pendrive/pd4' component={PenDriveHomepage} /> */}
-
-
-        {/* <Redirect to='/' /> */}
+        <Redirect to='/' />
       </Switch>
       <Footer />
     </div>
