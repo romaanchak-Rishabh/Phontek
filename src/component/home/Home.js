ddeea98aa0c5391156fad5ab3f8f9790bd8608b4
Rishabh Tater <rishabhtater14@gmail.com>
2021-11-13 22:55:30 +0530

    added helmet for home

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import "./Home.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import {Helmet} from "react-helmet";
 
 import one from "../../assets/homepage_carousel/1.jpg";
 import two from "../../assets/homepage_carousel/2.jpg";
@@ -17,6 +18,27 @@ const Home = () => {
 
     return (
         <div className='homepage-main'>
+        <Helmet>
+            <title>Phontek - Your Technological Companion</title>
+            <meta name="description" content="Visit Phontek and explore all best day-to-day gadgets to make your live more easy and better."/>
+            <link rel="canonical" href="https://phontek.in/"/>
+            <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"/>
+            <meta name="keywords" content="Phontek, Phontek.in, mobile accessories, mobile accessories near me, mobile accessories store near me, mobile accessories wholesale, mobile accessories store, mobile accessories display rack, mobile accessories wholesale near me, mobile accessories list, mobile accessories market, mobile accessories website, earphone accessories, earphone accessories name, earphone accessories earbuds, earphone accessories shop, earphone accessories mobile, earphone accessories stand, earphone accessories airpods, earphone accessories radio" />
+            
+            {/* Open Graph data */}
+            <meta property="og:locale" content="en_US" />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content="Phontek - Your Technological Companion" />
+            <meta property="og:description" content="Visit Phontek and explore all best day-to-day gadgets to make your live more easy and better." />
+            <meta property="og:url" content="https://phontek.in/" />
+            <meta property="og:site_name" content="Phontek" />
+            <meta property="og:updated_time" content="2021-11-11T00:00:00+05:30" />
+            <meta property="og:image" content="	data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAANwA…tMDItMDFUMDg6NDY6NDArMDA6MDDTu/O8AAAAAElFTkSuQmCC" />
+            <meta property="product:brand" content="Phontek" />
+            <meta property="product:price:currency" content="INR" />
+            {/* Open Graph data */}
+        </Helmet>
+
             <div className='image-carousel'>
             <Carousel infiniteLoop={true} autoPlay={true} interval={3000} transitionTime={500} showArrows={false} showStatus={false} showThumbs={false} showIndicators={false}>
                 <div>
